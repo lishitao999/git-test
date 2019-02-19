fcea7d5793285b6ebeff97f24b0f078f459d25f6
zhangsan <zhangsan@itcast.cn>
2019-02-19 19:05:21 +0800

    添加一个Second类

---
+++ git-test-project/src/com/itheima/git/Second.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+/**
+ * @Author: com.itheima
+ * @Date: 2019/2/19 19:03
+ */
+public class Second {
+
+    public void test(){
+
+        System.out.println("hello second");
+    }
+}
